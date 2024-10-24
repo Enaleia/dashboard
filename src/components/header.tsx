cf5e2215da9c71427e0f7af7092ffd835549de53
CJ Rose <cjrose59@gmail.com>
2024-10-23 17:37:08 -0700

    feat: mobile nav layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,95 @@
 import { Link } from '@tanstack/react-router'
+import { useState } from 'react';
+import { Menu, X } from 'lucide-react'
 
 const NavBar = () => {
 	return (
-		<div className="">
+		<div>
 			<MobileNavBar />
 			<DesktopNavBar />
 		</div>
 	);
 };
 
-const MobileNavBar = () => {
+const NavMenu = ({ closeMenu }: { closeMenu: () => void }) => {
 	return (
-		<nav className='md:hidden'>
-      
+		<div className='fixed z-3 top-0 left-0 w-full h-full bg-black opacity-80 px-10 py-16'>
+      <div className='flex flex-col gap-8 items-end text-white text-xl'>
+        <X color="white" size={40} onClick={closeMenu}/>
+        <div>
+          <Link
+            to="/locations"
+            onClick={closeMenu}
+            activeProps={{
+              className: 'font-bold',
+            }}
+          >
+            Locations
+          </Link>
+        </div>
+        <div>
+          <Link
+            to="/vessels"
+            onClick={closeMenu}
+            activeProps={{
+              className: 'font-bold',
+            }}
+          >
+            Vessels
+          </Link>
+        </div>
+        <div>
+          <Link
+            to="/economy"
+            onClick={closeMenu}
+            activeProps={{
+              className: 'font-bold',
+            }}
+          >
+            Circular Economy
+          </Link>
+        </div>
+        <div>
+          <Link
+            to="/about"
+            onClick={closeMenu}
+            activeProps={{
+              className: 'font-bold',
+            }}
+          >
+            About
+          </Link>
+        </div>
+        <div>
+          <a 
+            onClick={closeMenu}
+            href='https://optimism.easscan.org/'
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            EAS Explorer ↗️
+          </a>
+        </div>
+      </div>
+		</div>
+	);
+};
+
+const MobileNavBar = () => {
+  const [isMenuOpened, setIsMenuOpened] = useState(false)
+	return (
+		<nav className='md:hidden flex justify-between p-6'>
+      <Link
+        to="/"
+        activeProps={{
+          className: 'font-bold',
+        }}
+        activeOptions={{ exact: true }}
+      >
+        Enaleia
+      </Link>
+      <Menu onClick={() => setIsMenuOpened(true)} />
+      {isMenuOpened && <NavMenu closeMenu={() => setIsMenuOpened(false)} />}
     </nav>
   )
 }
@@ -30,7 +107,7 @@ const DesktopNavBar = () => {
             activeOptions={{ exact: true }}
           >
             Enaleia
-          </Link>{' '}
+          </Link>
         </div>
         <div className='flex gap-16'>
           <Link
