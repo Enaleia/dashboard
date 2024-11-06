c0ac3047987c62bc0aad4b8a9301be4b00287119
CJ Rose <cjrose59@gmail.com>
2024-11-06 10:41:46 -0700

    style: locations table

--- src/components/locations-table.tsx
+++ src/components/locations-table.tsx
@@ -17,11 +17,11 @@ const LocationsTable = () => {
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Country</TableHead>
-            <TableHead>Coordinates</TableHead>
-            <TableHead>Type</TableHead>
-            <TableHead>Action count</TableHead>
+            <TableHead className="p-0"><div className="text-xs font-bold text-black mt-2 mb-5 px-8 py-2 border border-black rounded-l-3xl">NAME</div></TableHead>
+            <TableHead className="p-0"><div className="text-xs font-bold text-black mt-2 mb-5 px-8 py-2 border-y border-black">COUNTRY</div></TableHead>
+            <TableHead className="p-0"><div className="text-xs font-bold text-black mt-2 mb-5 px-8 py-2 border border-black">COORDINATES</div></TableHead>
+            <TableHead className="p-0"><div className="text-xs font-bold text-black mt-2 mb-5 px-8 py-2 border-y border-black">TYPE</div></TableHead>
+            <TableHead className="p-0"><div className="text-xs font-bold text-black mt-2 mb-5 px-8 py-2 border border-black rounded-r-3xl">ACTION COUNT</div></TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -31,11 +31,11 @@ const LocationsTable = () => {
               onClick={() => navigate({to: `/locations/${location.name}`})}
               className="cursor-pointer hover:font-bold"
             >
-              <TableCell>{location.name}</TableCell>
-              <TableCell>{location.country}</TableCell>
-              <TableCell>{String(location.latitude).slice(0, 10)}, {String(location.longitude).slice(0, 10)} </TableCell>
-              <TableCell>{location.type}</TableCell>
-              <TableCell>{location.actions}</TableCell>            
+              <TableCell className="p-0"><div className="mb-2 px-8 py-5 border border-black rounded-l-3xl">{location.name}</div></TableCell>
+              <TableCell className="p-0"><div className="mb-2 px-8 py-5 border-y border-black">{location.country}</div></TableCell>
+              <TableCell className="p-0"><div className="mb-2 px-8 py-5 border border-black">{String(location.latitude).slice(0, 10)}, {String(location.longitude).slice(0, 10)}</div></TableCell>
+              <TableCell className="p-0"><div className="mb-2 px-8 py-5 border-y border-black flex gap-2"><img src={`/${location.type}_icon.svg`} alt="location icon"/><span>{location.type}</span></div></TableCell>
+              <TableCell className="p-0"><div className="mb-2 px-8 py-5 border border-black rounded-r-3xl">{location.actions}</div></TableCell>            
             </TableRow>
           ))}
         </TableBody>
