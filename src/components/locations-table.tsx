75ded122f8f77ff3b675e30ffe9f916d2cdcfecf
CJ Rose <cjrose59@gmail.com>
2024-11-06 10:48:43 -0700

    style: add flags to table

--- src/components/locations-table.tsx
+++ src/components/locations-table.tsx
@@ -32,7 +32,7 @@ const LocationsTable = () => {
               className="cursor-pointer hover:font-bold"
             >
               <TableCell className="p-0"><div className="mb-2 px-8 py-5 border border-black rounded-l-3xl">{location.name}</div></TableCell>
-              <TableCell className="p-0"><div className="mb-2 px-8 py-5 border-y border-black">{location.country}</div></TableCell>
+              <TableCell className="p-0"><div className="mb-2 px-8 py-5 border-y border-black flex gap-2"><img src={`/flag_${location.country}.svg`} alt="country flag"/><span>{location.country}</span></div></TableCell>
               <TableCell className="p-0"><div className="mb-2 px-8 py-5 border border-black">{String(location.latitude).slice(0, 10)}, {String(location.longitude).slice(0, 10)}</div></TableCell>
               <TableCell className="p-0"><div className="mb-2 px-8 py-5 border-y border-black flex gap-2"><img src={`/${location.type}_icon.svg`} alt="location icon"/><span>{location.type}</span></div></TableCell>
               <TableCell className="p-0"><div className="mb-2 px-8 py-5 border border-black rounded-r-3xl">{location.actions}</div></TableCell>            
