1bc95adb96489ecc957732d8ecf4ba3ed8b6ff9f
CJ Rose <cjrose59@gmail.com>
2024-11-11 16:18:35 -0700

    feat: scroll to top on route change

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,10 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { NavHeader } from '../components/nav-header'
-import { NavFooter } from '../components/nav-footer'
+import { NavHeader } from '@/components/nav-header'
+import { ScrollToTop } from '@/components/scroll-to-top'
+import { NavFooter } from '@/components/nav-footer'
 import '@fontsource-variable/dm-sans'
-import '../index.css'
+import '@/index.css'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -13,6 +14,7 @@ function RootComponent() {
   return (
     <>
       <NavHeader />
+      <ScrollToTop />
       <Outlet />
       <NavFooter />
       <TanStackRouterDevtools position="bottom-right" />
