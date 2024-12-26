d6402d73e5ab93659efae2c8bae2a2ec96861cd2
CJ Rose <cjrose59@gmail.com>
2024-12-26 16:25:05 -0700

    query homePageStats

--- src/components/stats-bar.tsx
+++ src/components/stats-bar.tsx
@@ -1,4 +1,5 @@
-import { Separator } from "@radix-ui/react-select";
+import { useQuery } from "@tanstack/react-query";
+
 
 const landingStats = [
   {
@@ -62,33 +63,49 @@ const locationsStats = [
 
 interface StatCardProps {
   title: string;
-  stat: number;
-  isWeight: boolean;
-  description: string
+  value: number;
+  // isWeight: boolean;
+  // description: string
 }
 
-const StatCard = ({ title, stat, isWeight, description, borderClass }: StatCardProps & { borderClass: string }) => {
+const StatCard = ({ title, value, borderClass }: StatCardProps & { borderClass: string }) => {
   return (
     <div className={`flex flex-col w-full md:w-[20%] justify-center items-center text-center font-extralight ${borderClass} md:border-none md:pb-0`}>
       <p className="text-xl md:text-lg font-medium">{title}</p>
-      <p className="text-4xl md:text-5xl font-bold pt-4 pb-1">{stat}{isWeight && <span>K</span>}</p>
-      <p className="md:min-h-[30px] text-xs md:text-xs w-[80%] leading-tight md:leading-tight">{description}</p>
+      <p className="text-4xl md:text-5xl font-bold pt-4 pb-1">{value}</p>
+      <p className="md:min-h-[30px] text-xs md:text-xs w-[80%] leading-tight md:leading-tight">some description</p>
     </div>
   )
 }
 
 const StatsBar = ({ pageId }: {pageId: string}) => {
-  let pageStats: StatCardProps[]
-  if (pageId === "landing") {
-    pageStats = landingStats
-  } else {
-    pageStats = locationsStats
-  }
+
+  // let pageStats: StatCardProps[]
+  // if (pageId === "landing") {
+  //   pageStats = landingStats
+  // } else {
+  //   pageStats = locationsStats
+  // }
+
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ['homePageStats'],
+    queryFn: async () => {
+      const response = await fetch(
+        '/api/flows/trigger/352a7482-4a18-4484-a53b-78c381d4db61'
+        // 'https://hq.enaleia-hub.com/flows/trigger/352a7482-4a18-4484-a53b-78c381d4db61',
+      )
+      return await response.json()
+    },
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
 
   return (
     <article className='flex flex-col md:flex-row justify-around gap-8 md:gap-0 items-center px-12 pb-12 md:py-8 md:px-2'>
-      {pageStats.map((stat: StatCardProps, index: number) => (
-        <StatCard key={stat.title} title={stat.title} stat={stat.stat} isWeight={stat.isWeight} description={stat.description} borderClass={index === pageStats.length - 1? "" : "border-b border-black pb-6"}/>
+      {data.home_page_statistics.map((stat: StatCardProps, index: number) => (
+        <StatCard key={stat.title} title={stat.title} value={stat.value} borderClass={index === data.home_page_statistics.length - 1? "" : "border-b border-black pb-6"}/>
       ))}
     </article>
   )
