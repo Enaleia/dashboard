39c58b250ef3773eb03bc29e4fd1bd7b9382b640
CJ Rose <cjrose59@gmail.com>
2024-12-26 15:26:45 -0700

    add tanstack query

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,7 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { NavHeader } from '@/components/nav-header'
 import { ScrollToTop } from '@/components/scroll-to-top'
 import { NavFooter } from '@/components/nav-footer'
@@ -10,14 +12,17 @@ export const Route = createRootRoute({
   component: RootComponent,
 })
 
+const queryClient = new QueryClient()
+
 function RootComponent() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <NavHeader />
       <ScrollToTop />
       <Outlet />
       <NavFooter />
-      <TanStackRouterDevtools position="bottom-right" />
-    </>
+      <TanStackRouterDevtools position="bottom-left" />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
