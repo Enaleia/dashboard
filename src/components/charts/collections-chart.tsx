0a8913df0b6a41be663c61d5fa1bf4f1f68ad54f
CJ Rose <cjrose59@gmail.com>
2025-02-05 14:30:29 -0700

    charts: handle loading/error state

--- src/components/charts/collections-chart.tsx
+++ src/components/charts/collections-chart.tsx
@@ -23,10 +23,6 @@ const CollectionsChart = ({ pageName, partnerId, timeRange }: CollectionsChartPr
   const chartConfig = pageName === "Home" ? MaterialsChartConfig : ActivitiesChartConfig
   const { ticks, tickFormatter } = useChartTicks(records, timeRange)
 
-  if (isPending) return <div className="w-full h-[490px] p-28">Loading...</div>
-  if (error) return 'An error has occurred: ' + error.message
-
-
   return (
     <ScrollArea className="max-w-[350px] md:max-w-[1500px]">
       <Card className="border-none shadow-none"> 
@@ -34,70 +30,82 @@ const CollectionsChart = ({ pageName, partnerId, timeRange }: CollectionsChartPr
           <ChartContainer 
             config={chartConfig} 
             className=" w-full min-h-[400px] max-h-[400px]"
-          >         
-            <AreaChart
-              accessibilityLayer
-              data={records}
-              margin={{ right: 30 }}
-            >
-              <CartesianGrid vertical={false} />
-              <XAxis
-                dataKey="date"
-                tickLine={false}
-                axisLine={false}
-                tickMargin={8}
-                ticks={ticks}              
-                tickFormatter={tickFormatter}
-                interval={0}
-              />
-              <YAxis
-                tickLine={false}
-                axisLine={false}
-                tickMargin={8}
-                tickCount={4}
-              />
-              <ChartTooltip 
-                cursor={false} 
-                content={
-                  <ChartTooltipContent 
-                    className="w-[250px] rounded-3xl gap-2 md:gap-3 text-sm md:text-lg p-4 md:p-6"
-                    labelFormatter={(value) => (
-                      <div className="font-extralight">
-                        {new Date(value).toLocaleDateString("en-US", { month: "short",  year: "numeric" })}
-                      </div>
-                    )}
-                    formatter={(value, name, item, index) => (
-                      <>
-                        <div className={`h-3 w-3 md:h-4 md:w-4 rounded-full bg-${name}`}/>
-                        <div className="font-semibold text-sm">{formatCamelCaseString(String(name))}</div>
-                        <div className="ml-auto font-extralight text-sm">{value} Kgs</div>
-                        {index === (pageName === "Home" ? 6 : 3) && (
-                          <div className="mt-1.5 flex basis-full items-center border-t border-gray-400 pt-1.5 text-sm md:text-lg">
-                            Total
-                            <div className="ml-auto font-extralight">
-                              {calculateTooltipTotal({payload: item.payload, config: chartConfig})} Kgs
-                            </div>
-                          </div>
-                        )}
-                      </>
-                    )}                    
-                  />
-                } 
-              />
-              {Object.keys(chartConfig).map((key) => (
-                <Area
-                  key={key}
-                  dataKey={key}
-                  type="monotone"
-                  baseValue={0}
-                  fill={`hsl(var(--${key}))`}
-                  fillOpacity={0.4}
-                  stroke={`hsl(var(--${key}))`}
-                  strokeWidth={3}
-                  connectNulls={true}
+          >
+            {isPending ? (
+              <div className="w-full h-full text-lg">
+                Loading chart data...
+              </div>
+            ) : error || records.length < 1 ? (
+              <div className="w-[40%] md:w-full h-full flex flex-col justify-center md:items-center text-center text-lg">
+                <p>😕 sorry!</p>
+                <p>We were not able to build the chart you requested.</p>
+                <img src="/Sealife/dolphin.svg" alt="dolphin illustration" className="w-[300px] h-[300px]"/>
+              </div>
+            ) : (
+              <AreaChart
+                accessibilityLayer
+                data={records}
+                margin={{ right: 30 }}
+              >
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="date"
+                  tickLine={false}
+                  axisLine={false}
+                  tickMargin={8}
+                  ticks={ticks}              
+                  tickFormatter={tickFormatter}
+                  interval={0}
                 />
-              ))}
-            </AreaChart>            
+                <YAxis
+                  tickLine={false}
+                  axisLine={false}
+                  tickMargin={8}
+                  tickCount={4}
+                />
+                <ChartTooltip 
+                  cursor={false} 
+                  content={
+                    <ChartTooltipContent 
+                      className="w-[250px] rounded-3xl gap-2 md:gap-3 text-sm md:text-lg p-4 md:p-6"
+                      labelFormatter={(value) => (
+                        <div className="font-extralight">
+                          {new Date(value).toLocaleDateString("en-US", { month: "short",  year: "numeric" })}
+                        </div>
+                      )}
+                      formatter={(value, name, item, index) => (
+                        <>
+                          <div className={`h-3 w-3 md:h-4 md:w-4 rounded-full bg-${name}`}/>
+                          <div className="font-semibold text-sm">{formatCamelCaseString(String(name))}</div>
+                          <div className="ml-auto font-extralight text-sm">{value} Kgs</div>
+                          {index === (pageName === "Home" ? 6 : 3) && (
+                            <div className="mt-1.5 flex basis-full items-center border-t border-gray-400 pt-1.5 text-sm md:text-lg">
+                              Total
+                              <div className="ml-auto font-extralight">
+                                {calculateTooltipTotal({payload: item.payload, config: chartConfig})} Kgs
+                              </div>
+                            </div>
+                          )}
+                        </>
+                      )}                    
+                    />
+                  } 
+                />
+                {Object.keys(chartConfig).map((key) => (
+                  <Area
+                    key={key}
+                    dataKey={key}
+                    type="monotone"
+                    baseValue={0}
+                    fill={`hsl(var(--${key}))`}
+                    fillOpacity={0.4}
+                    stroke={`hsl(var(--${key}))`}
+                    strokeWidth={3}
+                    connectNulls={true}
+                  />
+                ))}
+              </AreaChart> 
+            )}           
           </ChartContainer>       
         </CardContent>
       </Card>
