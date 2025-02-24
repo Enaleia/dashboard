14653a9779f9fbeb6d62b71820ae178ced0a5123
CJ Rose <cjrose59@gmail.com>
2025-02-24 11:59:59 -0800

    update Metadata layout

--- src/components/products/Metadata.tsx
+++ src/components/products/Metadata.tsx
@@ -17,7 +17,7 @@ interface MetadataProps {
 const Metadata = ({ productId, dataCategory }: MetadataProps) => {
    // Fetch product data from API with loading/error states
   const { isPending, error, data } = useProductData({ productId, dataCategory })
-  const { stats = {}, materials = {} } = data?.meta ?? {}
+  const { stats = {}, plastics = {} } = data?.meta ?? {}
 
   /*
   * Loading/Error state view
@@ -39,21 +39,19 @@ const Metadata = ({ productId, dataCategory }: MetadataProps) => {
     <article className='pt-2'>
       {/* Render each stat title and its value */}
       {Object.entries(stats).map(([title, value], index) => (
-        <div key={title}>
-          <div className={`flex justify-between text-sm ${index > 0 ? 'border-t border-darkSand' : ''} py-2`}>          
-            <p>{title}</p>
-            <p className='font-bold'>{value as string | number}<span className='font-extralight'>{typeof value === 'number' ? ' kg' : ''}</span></p>
-          </div>
-
-          {/* Render materials list after the fourth stat */}
-          {index === 3 && Object.entries(materials).map(([material, value], index, arr) => (
-            <div key={material} className={`flex justify-between text-sm pl-2 ${index === arr.length - 1 ? 'pb-2' : ''}`}>
-              <p className='font-extralight'>{material}</p>
-              <p className='font-bold'>{value as number}<span className='font-extralight'>{typeof value === 'number' ? ' kg' : ''}</span></p>
-            </div>
-          ))}
+        <div key={title} className={`flex justify-between text-sm ${index > 0 ? 'border-t border-darkSand' : ''} py-2`}>          
+          <p>{title}</p>
+          <p className='font-bold'>{value as string | number}<span className='font-extralight'>{typeof value === 'number' ? ' kg' : ''}</span></p>
         </div>
       ))}
+      {/* Render recycled plastics breakdown */}
+      {Object.entries(plastics).map(([plastic, value]) => (
+        <div key={plastic} className='flex justify-between text-sm pl-2 pt-2'>
+          <p className='font-extralight'>{plastic}</p>
+          <p className='font-bold'>{value as number}</p>
+        </div>
+      ))}
+
     </article>
   )
 }
